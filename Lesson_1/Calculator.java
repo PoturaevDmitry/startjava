d430282287cb5cbf5ec90b30c6882b77b7bb2499
Dmitry <dimas66@mail.ru>
2022-09-23 20:10:08 +0500

    Итоговые домашние задания

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        char sign = '+';
+        int num1 = 10;
+        int num2 = 2;
+        int result = 0;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+        } else if (sign == '%') {
+            result = num1 % num2;
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
