ecf44a1a426d63d83f5f15f017c88160ca3c5c11
Dmitry <dimas66@mail.ru>
2022-09-24 18:49:57 +0500

    Исправление замечаний в итоговом ДЗ

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,12 +2,9 @@ public class MyFirstGame {
 
     public static void main(String[] args) {
         int secretNum = 83;
-        int playerNum = 0;
+        int playerNum = 101 - secretNum;
         System.out.println("Угадайте число от 1 до 100.");
         do {
-            if (playerNum == 0) {
-                playerNum = 101 - secretNum;
-            }
             if (playerNum > secretNum) {
                 System.out.println("число " + playerNum + " больше того, что загадал компьютер");
                 playerNum -= (playerNum - secretNum > 10) ? 10 : 1;
