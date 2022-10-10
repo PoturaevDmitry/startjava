b117c5d49ea94c08a4bf12c2954a418a41d9b61e
Dmitry <dimas66@mail.ru>
2022-10-10 22:32:22 +0500

    Итоговые ДЗ для Lesson_4
    - программа "Угадай число", повторное исправление замечаний

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,15 +1,19 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Scanner;
+
 public class GuessNumber {
 
     public static final int MAX_ATTEMPTS = 10;
     private final Player player1;
     private final Player player2;
+    private final Scanner scanner;
     private Player currentPlayer;
 
     public GuessNumber(String namePlayer1, String namePlayer2) {
         player1 = new Player(namePlayer1, MAX_ATTEMPTS);
         player2 = new Player(namePlayer2, MAX_ATTEMPTS);
+        scanner = new Scanner(System.in);
     }
 
     public void start() {
@@ -19,13 +23,13 @@ public class GuessNumber {
         System.out.println("У каждого игрока по " + MAX_ATTEMPTS + " попыток");
         currentPlayer = player1;
         while (true) {
-            if (exceedNumPlayerAttempts()) break;
+            if (!hasAttempts()) break;
             System.out.print("Ход игрока " + currentPlayer.getName() + ": ");
-            if (checkGuess(currentPlayer.guess(), secretNum)) break;
+            if (compareNums(guess(), secretNum)) break;
             changePlayer();
         }
-        showPlayerResults(player1);
-        showPlayerResults(player2);
+        showPlayerAttempts(player1);
+        showPlayerAttempts(player2);
     }
 
     private void init() {
@@ -33,15 +37,21 @@ public class GuessNumber {
         player2.reset();
     }
 
-    private boolean exceedNumPlayerAttempts() {
+    private boolean hasAttempts() {
         if (currentPlayer.getCountAttempts() == MAX_ATTEMPTS) {
             System.out.println("У " + currentPlayer.getName() + " закончились попытки");
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
-    private boolean checkGuess(int playerNum, int secretNum) {
+    private int guess() {
+        int playerNum = scanner.nextInt();
+        currentPlayer.addEnteredNum(playerNum);
+        return playerNum;
+    }
+
+    private boolean compareNums(int playerNum, int secretNum) {
         if (playerNum == secretNum) {
             System.out.println("Игрок " + currentPlayer.getName() +  " угадал число " +
                     secretNum + " с " + currentPlayer.getCountAttempts() + " попытки");
@@ -56,7 +66,7 @@ public class GuessNumber {
         currentPlayer = currentPlayer == player1 ? player2 : player1;
     }
 
-    private void showPlayerResults(Player player) {
+    private void showPlayerAttempts(Player player) {
         System.out.print("Игрок " + player.getName() + " ввел числа: " );
         for (int num : player.getEnteredNums()) {
             System.out.print(num + " ");
--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,7 +1,6 @@
 package com.startjava.lesson_2_3_4.guess;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Player {
 
@@ -18,27 +17,20 @@ public class Player {
         return name;
     }
 
+    public int[] getEnteredNums() {
+        return Arrays.copyOf(enteredNums, countAttempts);
+    }
+
     public int getCountAttempts() {
         return countAttempts;
     }
 
     public void reset() {
-        if (countAttempts > 0) Arrays.fill(enteredNums, 0, countAttempts - 1, 0);
+        Arrays.fill(enteredNums, 0, countAttempts, 0);
         countAttempts = 0;
     }
 
-    public int[] getEnteredNums() {
-        return Arrays.copyOf(enteredNums, countAttempts);
-    }
-
-    public int guess() {
-        Scanner scanner = new Scanner(System.in);
-        int playerNum = scanner.nextInt();
-        setEnteredNum(playerNum);
-        return playerNum;
-    }
-
-    private void setEnteredNum(int enteredNum) {
+    public void addEnteredNum(int enteredNum) {
         enteredNums[countAttempts++] = enteredNum;
     }
 }
